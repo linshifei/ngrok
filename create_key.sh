97cdb95b95224595c514458622a99ea646f57b60
ubuntu <hacker@vip.qq.com>
2018-02-18 22:36:55 +0800

    add sh

---
+++ create_key.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#export NGROK_DOMAIN="adshonor.com"
+export NGROK_DOMAIN="118.89.173.180"
+
+openssl genrsa -out base.key 2048
+openssl req -new -x509 -nodes -key base.key -days 10000 -subj "/CN=$NGROK_DOMAIN" -out base.pem
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key -subj "/CN=$NGROK_DOMAIN" -out server.csr
+openssl x509 -req -in server.csr -CA base.pem -CAkey base.key -CAcreateserial -days 10000 -out server.crt
+
+cp base.pem assets/client/tls/ngrokroot.crt  
+cp server.crt assets/server/tls/snakeoil.crt  
+cp server.key assets/server/tls/snakeoil.key  
